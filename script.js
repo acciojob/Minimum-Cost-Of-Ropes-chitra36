63e569b9af43a8733ab730c3020d81df61fd2c79
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-14 04:34:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function minCostToFormRope(a){
 	var inputElement=document.querySelector('input').value;
 	var arr=inputElement.split(',');
 	arr.sort(function(a,b{return a-b});
-	var cost =0;{
+	var cost =0;
+	while(arr.length>1){
 		var res=Number(arr[0])+Number(arr[1]);
 	arr.splice90,2);
 	arr.push(res);
