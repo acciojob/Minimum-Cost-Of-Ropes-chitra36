4bf9cf02090d2767018cdd957e22d011e8351264
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-18 16:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-//your code here
-function minCostToFormRope(a){
-	a.preventDefault();
-	var inputElement=document.querySelector('input').value;
-	var arr=inputElement.split(',');
-	arr.sort(function(a,b){return a-b});
-	var cost =0;
+function calculateMinCost() {
+  //your code here
+  // read data from input element;
+  var inputData = document.querySelector("#rope-lengths").value;
+  var inputArr = inputData.split(",");
 
-	while(arr.length>1){
-		var res=Number(arr[0])+Number(arr[1]);
-	arr.splice(0,2);
-	arr.push(res);
-	cost+=res;
-	arr.sort(function(a,b){return a-b});
-	}
-document.getElementById("result").textContent=cost;
+  for (var i = 0; i < inputArr.length; i++) {
+    inputArr[i] = Number(inputArr[i]);
+  }
+  var cost = 0;
+
+  while (inputArr.length > 1) {
+    var newRope = inputArr[0] + inputArr[1];
+    cost += newRope;
+
+    // delete first two element
+    inputArr.splice(0, 2);
+    inputArr.push(newRope);
+    //alert(newRope);
+  }
+  document.querySelector("#result").textContent = cost;
 }
